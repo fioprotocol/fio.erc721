11d5416719fe2b1b9a2c06ac1e5a9420ec697696
adsorptionenthalpy <androulidakisa@gmail.com>
2021-11-22 17:52:51 -0700

    Polygon network

---
+++ truffle-config.polygon.js
@@ -0,0 +1,61 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+// create a file at the root of your project and name it .env -- there you can set process variables
+// like the mnemomic and Infura project key below. Note: .env is ignored by git to keep your private information safe
+require('dotenv').config();
+const mnemonic = process.env["MNEMONIC"];
+const appid = process.env["APP_ID"];
+
+module.exports = {
+
+  /**
+  * contracts_build_directory tells Truffle where to store compiled contracts
+  */
+  contracts_build_directory: './build/polygon-contracts',
+
+  /**
+  * contracts_directory tells Truffle where the contracts you want to compile are located
+  */
+  contracts_directory: './contracts/polygon',
+
+
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    //polygon matic testnet
+    matic_mumbai_testnet: {
+      provider: () => new HDWalletProvider({
+        mnemonic: {
+          phrase: mnemonic
+        },
+        providerOrUrl:
+         "https://rpc-mumbai.maticvigil.com/v1/" + appid
+      }),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      chainId: 80001
+    }
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.0",
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+        enabled: true,
+        runs: 1
+        },
+      }
+    },
+  } 
+}
