3ad7fe9e943f052f9688c23989854ba28a991b72
BlockTomeLTD <BlockTomeLTD@gmail.com>
2022-01-25 18:01:47 -0700

    update sol

--- truffle-config.polygon.js
+++ truffle-config.polygon.js
@@ -49,7 +49,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.0",
+      version: "0.8.4",
       settings: {          // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
         enabled: true,
