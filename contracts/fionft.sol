ae3eeaf58cdca3519657b314df1ea3e062e4d14d
Adam Androulidakis <37982984+adsorptionenthalpy@users.noreply.github.com>
2022-07-15 07:21:10 -0600

    Update fionft.sol

--- contracts/fionft.sol
+++ contracts/fionft.sol
@@ -3,7 +3,7 @@
 // Adam Androulidakis 3/2021
 // Prototype: Do not use in production
 
-pragma solidity 0.8.0;
+pragma solidity 0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
