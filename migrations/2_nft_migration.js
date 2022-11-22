f4a2a2daca75ffec316b1d24036a71bc8a822f93
Eric Butz <ericgbutz@gmail.com>
2022-11-22 08:55:15 -0700

    Truffle config updates for mainnet polygon release

--- migrations/2_nft_migration.js
+++ migrations/2_nft_migration.js
@@ -1,16 +1,19 @@
 const FIONFT = artifacts.require("FIONFT");
 
-const { CUSTODIANS_LOCAL, CUSTODIANS_DEVNET, CUSTODIANS_TESTNET } = process.env;
+const { CUSTODIANS_LOCAL, CUSTODIANS_DEVNET, CUSTODIANS_TESTNET, CUSTODIANS_MAINNET } = process.env;
 const custodiansLocal = CUSTODIANS_LOCAL.split(',');
 const custodiansDevnet = CUSTODIANS_DEVNET.split(',');
 const custodiansTestnet = CUSTODIANS_TESTNET.split(',');
+const custodiansMainnet = CUSTODIANS_MAINNET.split(',');
 
 module.exports = async function (deployer, network) {
   if (network == "development") {
     await deployer.deploy(FIONFT, custodiansLocal);
   } else if (network == "mumbai_devnet") {
     await deployer.deploy(FIONFT, custodiansDevnet);
-  } else if (network == "mumbai_testnet" | network == "goerli_testnet") {
+  } else if (network == "mumbai_testnet" || network == "goerli_testnet") {
     await deployer.deploy(FIONFT, custodiansTestnet);
+  } else if (network == "polygon") {
+    await deployer.deploy(FIONFT, custodiansMainnet);
   }
 };
